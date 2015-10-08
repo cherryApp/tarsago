cc9b5efc5913c4671f8b6fa052b297fc7b5d67cb
cherryApp <j.cserko@gmail.com>
2015-10-08 12:42:46 +0200

    DOM és JS

---
+++ js/dom.js
@@ -0,0 +1,20 @@
+// Szelektorok.
+var p = document.querySelector( "p" );
+console.log( p );
+
+// Kiolvassuk a css elérési utat a localStorage-ból és módosítjuk a linket.
+function changeCssSource( href ) {
+    if ( localStorage.userSettings ) {
+        var css = document.querySelector( "#main-css" )
+            .href = localStorage.userSettings;
+    }
+}
+
+
+// A szelekt módosítása esetén mentjük az új kiválasztott stílust.
+document.querySelector( "select[name='style-select']" )
+    .addEventListener( "change", function(e) {
+        console.log( e.target.value );
+        localStorage.userSettings = e.target.value;
+        changeCssSource();
+    });
