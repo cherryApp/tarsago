b69893377c6e0c56e5e22f0727e3aefea11d24da
cherryApp <j.cserko@gmail.com>
2015-10-12 11:28:52 +0200

    Preprocess

--- lista_oldal/js/main.js
+++ lista_oldal/js/main.js
@@ -6,36 +6,35 @@ tarsagoApp.controller( "termekController",
     ["$scope", "$http", function($scope, $http) {
         
         $scope.showingProducts = [];
-        $scope.productNum = 6;
+        $scope.productNum = 9;
         
-        // Termék ellenőrzés.
-        $scope.checkProduct = function( product ) {
-        
-            // Látható-e a termék?
-            var isQt = product.qt > 0;
+        // Termékek feldolgozása.
+        $scope.preProcess = function( products ) {
             
-            if ( isQt ) {
-                
-                if ( $scope.showingProducts.length < $scope.productNum ) {
-                    product.showing = true;
-                    if ( $scope.showingProducts.indexOf(product) === -1 )
-                        $scope.showingProducts.push( product );                    
+            // Megkeressük a készelten lévőket.
+            var processed = [];
+            var missing = [];
+            for ( var k in products ) {
+                if ( products[k].qt > 0 ) {
+                    processed.push( products[k] );                    
+                } else {
+                    products[k].missing = true;
+                    missing.push( products[k] );
                 }
-                
-            } else {
-                product.showing = false;
             }
             
-            // Ha nem látható, visszatérünk.
-            return true;
+            // Hozzávesszük azokat amikből nincs elég.
+            processed = processed.concat( missing );
             
+            return processed.splice( 0, $scope.productNum );
+        
         };
     
         // Lekérjük a jsont.
         $http.get( "json/products.json" )
             .success( function(d) {
                 console.log( d );
-                $scope.products = d;
+                $scope.products = $scope.preProcess( d );
             } )
             .error( function(d) {
                 console.error( "Error: ", d );
